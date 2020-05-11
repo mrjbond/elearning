c625ee3be53524f95180f89af23ba8f1fc586e2c
Slavo Vojacek <public@slavovojacek.com>
2020-05-11 19:07:34 +0100

    chore(Application): Update KV App to start KV.V2 and KV.V3

--- kv/lib/kv/application.ex
+++ kv/lib/kv/application.ex
@@ -7,12 +7,12 @@ defmodule KV.Application do
 
   def start(_type, _args) do
     children = [
-      {Task.Supervisor, name: TaskSupervisor}
-      # %{
-      #   id: KV.V2.Supervised,
-      #   start: {KV.V2, :start_link, [%{milk: 1, honey: 2}]}
-      # },
-      # {KV.V3, %{milk: 1, honey: 2}}
+      {Task.Supervisor, name: TaskSupervisor},
+      %{
+        id: KV.V2.Supervised,
+        start: {KV.V2, :start_link, [%{milk: 1, honey: 2}]}
+      },
+      {KV.V3, %{milk: 1, honey: 2}}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
